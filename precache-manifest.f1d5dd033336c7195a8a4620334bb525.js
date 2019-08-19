22eb4a8e7ba760b32a75e601f1d07a6cea88eafb
Jun <junkun007@aim.com>
2019-08-19 10:09:06 -0400

    Updates

---
+++ precache-manifest.f1d5dd033336c7195a8a4620334bb525.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "090fffac0070de1edc21",
+    "url": "/robofriends/static/js/main.d40bec2d.chunk.js"
+  },
+  {
+    "revision": "d5707197e22dd987e9d0",
+    "url": "/robofriends/static/js/2.b93257fc.chunk.js"
+  },
+  {
+    "revision": "090fffac0070de1edc21",
+    "url": "/robofriends/static/css/main.7f3bbd92.chunk.css"
+  },
+  {
+    "revision": "d5707197e22dd987e9d0",
+    "url": "/robofriends/static/css/2.aae060c8.chunk.css"
+  },
+  {
+    "revision": "40f27fb0e0d0a07283aab9344238d296",
+    "url": "/robofriends/index.html"
+  }
+];
