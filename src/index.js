81314c745ff2fbbf3c5c43df9f472c6b201a7571
Jun <junkun007@aim.com>
2019-08-19 15:49:57 -0400

    added PWA requirements

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
+import serviceWorker from './serviceWorker';
 import "tachyons";
 import { searchRobots, requestRobots } from './reducer';
 
@@ -17,8 +17,4 @@ const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 ReactDOM.render(<Provider store={store}> 
         <App/> 
     </Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker();
